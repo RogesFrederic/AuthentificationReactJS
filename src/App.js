70b8f20a92893bd7f3c2c19113ec0d8950bc1dc1
Frédéric Rogès <roges.frederic@gmail.com>
2021-08-31 10:55:48 +0200

    Gestion de l'authentification

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import './App.css';
 import {Login} from './Login/Login';
 import { Profile } from './Profile/Profile';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Navbar } from './Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
 
-function App() {
-  return (
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Projet Encadré</Link>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/login">Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/profile">Profil</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route>
-          Non mais c'est quoi cet URL
-        </Route>
-      </Switch>
-    </Router>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthentificate: false
+    }
+    this.handleDeconnexion = this.handleDeconnexion.bind(this)
+    this.handleConnexion = this.handleConnexion.bind(this)
+  }
+  
+  handleDeconnexion() {
+    this.setState({isAuthentificate: false})
+  }
+
+  handleConnexion() {
+    console.log("connexion")
+    this.setState({isAuthentificate: true})
+  }
+
+  render() {
+    return (
+      <Router>
+        <Navbar isAuthentificate={this.state.isAuthentificate} handleDeconnexion={this.handleDeconnexion} />
+        <Switch>
+          <Route path="/login">
+            <Login isAuthentificate={this.state.isAuthentificate} handleConnexion={this.handleConnexion} />
+          </Route>
+          <Route path="/profile">
+            <Profile isAuthentificate={this.state.isAuthentificate} />
+          </Route>
+          <Route>
+            Non mais c'est quoi cet URL
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,21 +1,75 @@
 import React from "react";
 import './Login.css';
+import Users from '../users.json';
+import { Redirect } from 'react-router-dom';
 
 export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {      
+            username: 'defaut',
+            password: '',
+            loginError: false
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeUsername = this.handleChangeUsername.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleChangeUsername(event) {
+        this.setState({username: event.target.value})
+    }
+
+    handleChangePassword(event) {
+        this.setState({password: event.target.value})
+    }
+
+    handleSubmit(event) {
+        Users.forEach((user) => {
+           if (user.username === this.state.username && user.password === this.state.password ) {
+               localStorage.setItem('current-user', user.username)
+               console.log("ici")
+               this.props.handleConnexion()
+           }
+           if (this.isAuthentificate) { console.log("authentifié") }
+           this.setState({loginError: true})
+        });
+        event.preventDefault()
+    }
+
+    redirect = () => {
+        if (this.isAuthentificate) {
+            return <Redirect to="/profile" />
+        }
+    }
     
+    displayLoginError = () => {
+        if (this.state.loginError) {
+            return <div className="alert alert-danger" role="alert">
+                Erreur d'authentification
+            </div>
+        }
+    }
+
     render() {
-        return <div class="mt-5">
-            <form className="w-50 m-auto">
+        this.isAuthentificate = this.props.isAuthentificate
+        return <div className="mt-5">
+            {this.redirect()}
+            <form className="w-50 m-auto" onSubmit={this.handleSubmit}>
+                <div className="mb-3">
+                    {this.displayLoginError()}
+                </div>
                 <div className="mb-3">
                     <label htmlFor="user" className="form-label">Utilisateur</label>
-                    <input type="email" className="form-control" id="user" aria-describedby="emailHelp" />
+                    <input type="text" value={this.state.username} onChange={this.handleChangeUsername} className="form-control" id="user" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Mot de passe</label>
-                    <input type="password" className="form-control" id="password" />
+                    <input type="password" value={this.state.password} onChange={this.handleChangePassword} className="form-control" id="password" />
                 </div>
                 <button type="submit" className="btn btn-primary">Valider</button>
             </form>
         </div>
-    }
+    }   
 }
